2de215b586bb0bc54c6d95ffee77603aa7cd52e9
CandeDeveloper <candeitarg@gmail.com>
2025-02-15 20:04:49 -0300

    Reto Insta

--- Instagram/Reto3.js
+++ Instagram/Reto3.js
@@ -1,7 +1,15 @@
-// En un array donde todos los numeros son duplicados, encuentra el que no
+// En un array donde todos los numeros son duplicados, encuentra el que no. Utilizar map
 
-// const map1 = array1.map((x) => x * 2);
 
-function noRepeat(){
-  
+function noRepeat(arr) {
+    let numero = 0;
+    arr.map(x => {
+        if (arr.filter(y => y === x).length === 1) {
+            numero = x;
+        }
+    });
+
+    return numero;
 }
+
+console.log(noRepeat([1, 1, 3, 4, 4]))
